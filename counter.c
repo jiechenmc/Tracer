7c56883787d9a5f2158214bc45965ec31ca6842a
Jie Chen <jiechenmc@gmail.com>
2024-05-17 21:13:09 -0400

    update

--- counter.c
+++ counter.c
@@ -2,7 +2,11 @@
 
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
-#include <arpa/inet.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/udp.h>
+#include <linux/in.h>
+#include <asm-generic/types.h>
 
 struct
 {
@@ -11,6 +15,36 @@ struct
     __type(value, __u64);
     __uint(max_entries, 100);
 } pkt_count SEC(".maps");
+
+static __always_inline int parse_ip_src_addr(struct xdp_md *ctx, __u32 *ip_src_addr)
+{
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
+
+    // First, parse the ethernet header.
+    struct ethhdr *eth = data;
+    if ((void *)(eth + 1) > data_end)
+    {
+        return 0;
+    }
+
+    if (eth->h_proto != bpf_htons(ETH_P_IP))
+    {
+        // The protocol is not IPv4, so we can't parse an IPv4 source address.
+        return 0;
+    }
+
+    // Then parse the IP header.
+    struct iphdr *ip = (void *)(eth + 1);
+    if ((void *)(ip + 1) > data_end)
+    {
+        return 0;
+    }
+
+    // Return the source IP address in network byte order.
+    *ip_src_addr = (__u32)(ip->saddr);
+    return 1;
+}
 
 SEC("xdp")
 int xdp_pass(struct xdp_md *ctx)
@@ -26,7 +60,7 @@ int xdp_pass(struct xdp_md *ctx)
         struct ethhdr *eth = data;
         *count = data_end - data;
 
-        bpf_printk("packet size is %d", *count);
+        bpf_printk("packet size is %d", eth->h_dest);
     }
 
     return XDP_PASS;
